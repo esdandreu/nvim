bfa96a58f95d74cfeff5e41434fea2d5aabd84ff
andreu <andreu.gimenez@dotphoton.com>
2024-12-23 10:45:44 +0100

    Working in vscode

--- init.lua
+++ init.lua
@@ -1,3 +1,41 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.keymap.set('n', '<C-d>', 'mciw*<Cmd>nohl<CR>', { remap = true })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"},
+                           {"\nPress any key to exit..."}}, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({{
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}, {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {}
+}, {
+    "vscode-neovim/vscode-multi-cursor.nvim",
+    event = "VeryLazy",
+    cond = not not vim.g.vscode,
+    opts = {}
+}})
+
+-- vim.keymap.set({"n", "x", "i"}, "<C-d>", function()
+--     require("vscode-multi-cursor").addSelectionToNextFindMatch()
+-- end)
+
+vim.keymap.set('n', 'gb', 'mciw*<Cmd>nohl<CR>', {
+    remap = true
+})
